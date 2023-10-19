d98e865813737e378a5a023ce700970eafb9f205
KameguchiYu <yu.kameguchi8010@gmail.com>
2023-10-19 10:21:54 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023 1005 3/3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+static int mult(int a, int b)
+{
+	return a * b;
+}
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	cout << "”š‚ğ‚Q‚Â“ü—Í‚µ‚Ä‚­‚¾‚³‚¢" << endl;
+	cin >> a >> b;
+	cout << "Š|‚¯Z‚µ‚½“š‚¦‚Í" << mult(a, b) << "‚Å‚·" << endl;
+}
